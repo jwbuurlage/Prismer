9c373c260780efe79c07cf9f77f7ee8a42ec34be
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2015-05-18 20:23:42 +0200

    remove obsolete (for now) code from input

--- prismer/include/GameSessionInput.h
+++ prismer/include/GameSessionInput.h
@@ -14,23 +14,6 @@ class GameSessionInput
         void update(float elapsedTime);
 
     private: 
-        void mouseDown(Arya::MOUSEBUTTON button, bool buttonDown, int x, int y);
-        void mouseWheelMoved(int delta);
-        void mouseMoved(int x, int y, int dx, int dy);
-
-        //For key movement
-        bool goingForward, goingBackward, goingLeft, goingRight, goingUp, goingDown, rotatingLeft, rotatingRight;
-        bool mouseLeft, mouseRight, mouseTop, mouseBot; //whether mouse is at edge
-        bool draggingLeftMouse, draggingRightMouse;
-        bool slowMode; //Precise movement
-
-        void computeForce();
-
-        vec3 forceDirection;
-        vec3 specMovement;
-        vec3 specPos;
-
-        vec2 originalMousePos;
 };
 
-}
+} // namespace Prismer
