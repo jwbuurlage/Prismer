35bf15127f79c7d767cf2809975fe3f803db2723
Tom Bannink <tom@buurlagewits.nl>
2015-06-07 21:58:09 +0200

    Added circle primitive (20-gon)

--- src/Primitives.cpp
+++ src/Primitives.cpp
@@ -17,6 +17,7 @@ namespace Arya
         //  - quad (-1,-1,0) to (1,1,0)
         //  - hexagon
         //  - quad2d (-1,-1) to (1,1)
+        //  - circle (n-gon)
         // All z-coordinates are 0
 
         GLfloat triangleVertices[] = {
@@ -147,6 +148,45 @@ namespace Arya
         mesh = model->createMesh();
         mesh->geometry = geometry;
         addResource("quad2d", model);
+
+        const int ngon = 20;
+        GLfloat ngonVertices[ngon][3][3];
+
+        float prevAngle = 0.0f;
+        for (int i = 0; i < ngon; i++)
+        {
+            for (int j = 0; j < 3; j++) ngonVertices[i][j][2] = 0.0f;
+
+            float angle = (2.0f * M_PI * (i+1))/float(ngon);
+            ngonVertices[i][0][0] = 0.0f;
+            ngonVertices[i][0][1] = 0.0f;
+            ngonVertices[i][1][0] = cos(prevAngle);
+            ngonVertices[i][1][1] = sin(prevAngle);
+            ngonVertices[i][2][0] = cos(angle);
+            ngonVertices[i][2][1] = sin(angle);
+
+            prevAngle = angle;
+        }
+
+        geometry = make_shared<Geometry>();
+
+        geometry->primitiveType = GL_TRIANGLES;
+        geometry->vertexCount = ngon*3;
+        geometry->indexCount = 0;
+        geometry->frameCount = 1;
+
+        geometry->createVertexBuffer();
+        geometry->setVertexBufferData(sizeof(ngonVertices), ngonVertices);
+        geometry->createVAOs(1);
+        geometry->bindVAO(0);
+        geometry->setVAOdata(0, 3, 3 * sizeof(GLfloat), 0);
+        geometry->setVAOdata(1, 2, 3 * sizeof(GLfloat), 0);
+
+        model = make_shared<Model>(ModelTypeStatic);
+        model->shaderProgram = staticShader;
+        mesh = model->createMesh();
+        mesh->geometry = geometry;
+        addResource("circle", model);
     }
 }
 
