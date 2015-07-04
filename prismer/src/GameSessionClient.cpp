1fb29bd8f624852cfa2b0fd30eafaa5658d4bd0a
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2015-07-05 01:38:56 +0200

    add fps counter

--- prismer/src/GameSessionClient.cpp
+++ prismer/src/GameSessionClient.cpp
@@ -1,5 +1,7 @@
 #include <Arya.h>
 #include <memory>
+#include <sstream>
+#include <algorithm>
 
 #include "Faction.h"
 #include "FactionInput.h"
@@ -16,6 +18,7 @@
 namespace Prismer {
 
 using std::make_shared;
+using std::stringstream;
 
 GameSessionClient::GameSessionClient() : GameSession()
 {
@@ -47,6 +50,29 @@ bool GameSessionClient::init()
         fac->setInput(make_shared<FactionInput>(fac));
     }
 
+    /* FPS COUNTER */
+    using Arya::Material;
+    auto matGray = Material::create(vec4(0.8f, 0.8f, 0.8f, 0.8f));
+
+    using Arya::Font;
+    auto font = make_shared<Font>();
+    font->loadFromFile("DejaVuSansMono.ttf", 28);
+
+    using Arya::ImageView;
+    auto background = ImageView::create();
+    background->setMaterial(matGray);
+    background->setPosition(vec2(-1.0f, 1.0f), vec2(65.0f, -35.0f)); //middle-top + (0, -10px)
+    background->setSize(vec2(0.0f, 0.0f), vec2(110.0f, 50.0f)); //fullwidth + (-20px, +300px)
+    background->addToRootView();
+
+    using Arya::Label;
+    _fpsCounterLabel = Label::create();
+    _fpsCounterLabel->setPosition(vec2(0.0f, 0.0f), vec2(0.0f, 0.0f));
+    _fpsCounterLabel->setSize(vec2(0.0f, 0.0f), vec2(80.0f, 30.0f));
+    if (font)
+        _fpsCounterLabel->setFont(font);
+    _fpsCounterLabel->setText("0 FPS");
+    background->add(_fpsCounterLabel);
 
     return true;
 }
@@ -54,6 +80,19 @@ bool GameSessionClient::init()
 void GameSessionClient::update(float elapsedTime)
 {
     total_time += elapsedTime;
+
+    for (int i = 99; i >= 1; --i)
+        _elapsedTimes[i] = _elapsedTimes[i - 1];
+    _elapsedTimes[0] = elapsedTime;
+
+    auto sum_of_totals = std::accumulate(_elapsedTimes.begin(),
+            _elapsedTimes.end(), 0.0f);
+    auto average = sum_of_totals / 100.0f;
+
+    auto fps = (int)(1.0f / average);
+    stringstream ss;
+    ss << fps << " FPS";
+    _fpsCounterLabel->setText(ss.str());
 
     if (input)
         input->update(elapsedTime);
